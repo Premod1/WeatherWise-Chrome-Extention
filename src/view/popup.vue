4599cbe2cc2abaae65ab3e6483e30dc47b9d530b
premod1 <premodsuraweera1@gmail.com>
2023-04-07 01:44:17 +0530

    done

---
+++ src/view/popup.vue
@@ -0,0 +1,171 @@
+<script setup>
+import { ref, watchEffect } from "vue";
+
+const api_key = ref("08eca1f99895c2108e1d3f2fd2a0d136");
+const url_base = ref("https://api.openweathermap.org/data/2.5/");
+const query = ref("");
+const weather = ref({});
+
+async function fetchWeather(e) {
+  if (e.key == "Enter") {
+    const res = await fetch(
+      `${url_base.value}weather?q=${query.value}&units=metric&APPID=${api_key.value}`
+    );
+    const result = await res.json();
+    setResult(result);
+  }
+}
+function setResult(result) {
+  weather.value = result;
+}
+function dateBuilder() {
+  let d = new Date();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+  return `${day} ${date} ${month} ${year}`;
+}
+watchEffect(() => {
+  console.log(weather.value);
+});
+</script>
+<template>
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
+    <div class="main">
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="search......"
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">
+            {{ weather.name }},{{ weather.sys.country }}
+          </div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+#app {
+  margin: 0;
+  padding: 0;
+  width: 400px;
+  height: 570px;
+  background-image: url("../assets/cold-bg.jpg");
+
+  overflow: hidden;
+}
+#app.warm {
+  background-image: url("../assets/warm-bg.jpg");
+}
+.main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+.search-box {
+  width: 90%;
+  margin-bottom: 10px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 8px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
+
